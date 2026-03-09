507fefa920f4b397a2148d99a4dafe69ff118bb2
Sayo Adegoroye <steve.legion94@gmail.com>
2026-03-09 13:04:07 +0000

    fix- import issues

--- components/ui/form.tsx
+++ components/ui/form.tsx
@@ -1,8 +1,7 @@
 "use client";
 
 import * as React from "react";
-import { Label as LabelPrimitive } from "radix-ui";
-import { Slot } from "@radix-ui/react-slot";
+import { Label as LabelPrimitive, Slot } from "radix-ui";
 import {
   Controller,
   FormProvider,
@@ -104,12 +103,12 @@ function FormLabel({
   );
 }
 
-function FormControl({ ...props }: React.ComponentProps<typeof Slot>) {
+function FormControl({ ...props }: React.ComponentProps<typeof Slot.Root>) {
   const { error, formItemId, formDescriptionId, formMessageId } =
     useFormField();
 
   return (
-    <Slot
+    <Slot.Root
       data-slot="form-control"
       id={formItemId}
       aria-describedby={
